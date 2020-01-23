9694357da59fdad352554ce01a2714c2b8879247
Jack Baude <jackbaude@gmail.com>
2020-01-22 20:58:12 -0600

    added stuff

--- block.py
+++ block.py
@@ -1,25 +1,28 @@
 import hashlib
 import time
 class Block():
+    #constructor for each block
+    #data is abstarct and can be anything
     def __init__(self, index, timestamp, data, previous_hash):
         self.index = index
         self.timestamp = timestamp
         self.data = data
         self.previous_hash = previous_hash
-        
-        
-
+    #hash the block inorder to add it to the chain
     def hashBlock(self):
         key = hashlib.sha256()
-        key.update(str(self.index).encode('utf-8'))
-        key.update(str(self.timestamp).encode('utf-8'))
-        key.update(str(self.data).encode('utf-8'))
-        key.update(str(self.previous_hash).encode('utf-8'))
-        key.hexdigest()
-        return key
-        
-        
-        
-        
+        key.update(str(self.index) +
+               str(self.timestamp) +
+               str(self.data) +
+               str(self.previous_hash).encode('utf-8'))
+
+        return key.hexdigest()
+
+    def get_block(self):
+        return [self.index, self.timestamp, self.data, self.previous_hash]
+
+
+
+#test
 block = Block(0, time.time(), "john is fat", 0000)
-print(str(block.hashBlock().encode('utf-8')))
+#print(block.hashBlock())
--- chain.py
+++ chain.py
@@ -1,7 +1,42 @@
-class chain():
+import time
+from block import Block
+
+class Chain():
     def __init__(self):
         self.blocks = [self.get_genesis_block()]
-    
-    get_genesis_block(self):
-        return 
-        
+
+    def get_genesis_block(self):
+        return Block(0, time.time(), "Genesis Block", "0")
+
+    def get_prevblock(self, index):
+        return (self.blocks[index -1].hashBlock())
+
+    def add_block(self, data):
+        self.blocks.append(Block(len(self.blocks),
+                time.time(),
+                data,
+                self.get_prevblock(len(self.blocks) -1)))
+
+    def get_chain(self):
+        chain = []
+        for z in self.blocks:
+            print z.get_block()
+
+    def get_chain_length(self):
+        return (len(self.blocks))
+
+
+chain = Chain()
+chain.add_block("money given to jack")
+time.sleep(0.5)
+chain.add_block("money given to jack1")
+time.sleep(0.5)
+chain.add_block("money given to jack2")
+chain.add_block("money given to jack3")
+chain.add_block("money given to jack4")
+chain.add_block("money given to jack5")
+chain.add_block("money given to jack6")
+chain.add_block("money given to jack7")
+print("the length of the chain is {}". format(chain.get_chain_length()))
+
+print(chain.get_chain())
